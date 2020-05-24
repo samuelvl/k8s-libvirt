dea3b2863ad2877fcfc19d0c83962ce2304de837
Samuel Veloso <veloso.lopez.samuel@gmail.com>
2020-05-24 18:53:40 +0200

    Format docs

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,45 +1,38 @@
 # Load balancer
-output "load_balancers_info" {
-  value = libvirt_domain.load_balancer.network_interface.0.addresses
-}
-
-output "load_balancer_fqdn" {
-  value = libvirt_domain.load_balancer.network_interface.0.hostname
-}
-
-output "load_balancer_ssh" {
-  value = format("ssh -i src/ssh/maintuser/id_rsa maintuser@%s",
-      libvirt_domain.load_balancer.network_interface.0.hostname)
-}
-
-# Kubernetes masters
-output "kubernetes_masters_info" {
+output "kubernetes_load_balancer" {
   value = {
-    for master in libvirt_domain.kubernetes_master:
-    master.network_interface.0.hostname => master.network_interface.0.addresses
+    fqdn       = libvirt_domain.load_balancer.network_interface.0.hostname
+    ip_address = libvirt_domain.load_balancer.network_interface.0.addresses.0
+    ssh        = format("ssh -i src/ssh/maintuser/id_rsa maintuser@%s",
+      libvirt_domain.load_balancer.network_interface.0.hostname)
+    metrics   =  format("http://%s:5555/haproxy?stats",
+      libvirt_domain.load_balancer.network_interface.0.hostname)
   }
 }
 
-output "kubernetes_masters_ssh" {
+# Kubernetes cluster
+output "kubernetes_cluster" {
   value = {
-    for master in libvirt_domain.kubernetes_master:
-    master.name => format("ssh -i src/ssh/maintuser/id_rsa maintuser@%s",
-      master.network_interface.0.hostname)
+    api_server = format("https://api.%s:6443", var.dns.internal_zone.domain)
+  }
+}
+
+# Kubernetes masters
+output "kubernetes_masters" {
+  value = {
+    fqdn       = libvirt_domain.kubernetes_master.*.network_interface.0.hostname
+    ip_address = libvirt_domain.kubernetes_master.*.network_interface.0.addresses.0
+    ssh        = formatlist("ssh -i src/ssh/maintuser/id_rsa maintuser@%s",
+      libvirt_domain.kubernetes_master.*.network_interface.0.hostname)
   }
 }
 
 # Kubernetes workers
-output "kubernetes_workers_info" {
+output "kubernetes_workers" {
   value = {
-    for worker in libvirt_domain.kubernetes_worker:
-    worker.network_interface.0.hostname => worker.network_interface.0.addresses
-  }
-}
-
-output "kubernetes_workers_ssh" {
-  value = {
-    for worker in libvirt_domain.kubernetes_worker:
-    worker.name => format("ssh -i src/ssh/maintuser/id_rsa maintuser@%s",
-      worker.network_interface.0.hostname)
+    fqdn       = libvirt_domain.kubernetes_worker.*.network_interface.0.hostname
+    ip_address = libvirt_domain.kubernetes_worker.*.network_interface.0.addresses.0
+    ssh        = formatlist("ssh -i src/ssh/maintuser/id_rsa maintuser@%s",
+      libvirt_domain.kubernetes_worker.*.network_interface.0.hostname)
   }
 }
